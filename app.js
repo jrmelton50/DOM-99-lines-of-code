e8936aa4b7fa1bc6f7bd19c3a7d657fb06f9e42f
jrmelton50 <jrmelton@crimson.ua.edu>
2018-04-05 13:19:14 -0500

    Completed Assignment

--- app.js
+++ app.js
@@ -1,14 +1,40 @@
+var singButton = document.getElementById("button");
 var friends = ["Kaitlyn", "Josh", "Scott", "Alex", "Allison"];
 
-for (var i=0; i<friends.length; i++) {
-    var name = friends[i];
-    console.log(name + ":");
-    for (var j=99; j>0; j--) {
-        if (j == 1) {
-            console.log(j + " line of code in the file, " + j + " line of code; " + name + " strikes one out, clears it all out, no more lines of code in the file");
-        }
-        else {
-            console.log(j + " lines of code in the file, " + j + " lines of code; " + name + " strikes one out, clears it all out, " + (j-1) + " lines of code in the file");
+singButton.addEventListener("click", sing);
+
+function sing() {
+    for (var i = 0; i < friends.length; i++) {
+        var name = friends[i];
+        var div = createFriendDiv(name);
+        console.log(div);
+        for (var j = 99; j > 0; j--) {
+            displayLineOfSong(div, name, j);
         }
     }
+}
+
+function createFriendDiv(name) {
+    var div = document.createElement("div");
+    var h3 = document.createElement("h3");
+    var text = document.createTextNode(name);
+    h3.appendChild(text);
+    div.appendChild(h3);
+    div.className = "friend";
+    document.body.appendChild(div);
+    return div;
+}
+
+function displayLineOfSong(div, name, number) {
+    var lineToDisplay = document.createElement("p");
+    if (number == 1) {
+        var text = document.createTextNode(number + " line of code in the file, " + number + " line of code; " + name + " strikes one out, clears it all out, no more lines of code in the file");
+        // console.log(number + " line of code in the file, " + number + " line of code; " + name + " strikes one out, clears it all out, no more lines of code in the file");
+    }
+    else {
+        var text = document.createTextNode(number + " lines of code in the file, " + number + " lines of code; " + name + " strikes one out, clears it all out, " + (number - 1) + " lines of code in the file");
+        // console.log(number + " lines of code in the file, " + number + " lines of code; " + name + " strikes one out, clears it all out, " + (number - 1) + " lines of code in the file");
+    }
+    lineToDisplay.appendChild(text);
+    div.appendChild(lineToDisplay);
 }
